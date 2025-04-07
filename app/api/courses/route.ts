fd34833fbe114b2ecf0eb92080131ead5ab7ddbe
Shreyansh Jain <shreyanshjains1168@gmail.com>
2025-04-07 13:40:29 +0530

    minor bug

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -20,7 +20,6 @@ export async function GET(request: NextRequest) {
             },
             include: {
                 student: true,
-                courseMaterials: true,
                 schedules: true,
                 assignments: true
             },
